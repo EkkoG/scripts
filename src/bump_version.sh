38a40b9b60edba4e1bf148e7c738663165208eca
Ekko <beijiu572@gmail.com>
2023-09-18 16:55:35 +0800

    Update bump_version.sh

--- src/bump_version.sh
+++ src/bump_version.sh
@@ -7,25 +7,17 @@ new_version=$(($current_version + 1))
 echo "新版本号: $new_version"
 sed -i '' "s/CURRENT_PROJECT_VERSION = $current_version/CURRENT_PROJECT_VERSION = $new_version/g" $p_file
 
-if [ "$name_type" == "major" ]; then
-    current_version_name=$(grep -Eo 'MARKETING_VERSION = [0-9.]+' $p_file | grep -Eo '[0-9.]+' | tail -1)
-    echo "当前版本名: $current_version_name"
-    new_version_name=$(echo $current_version_name | awk -F. '{print $1+1".0.0"}')
-    echo "新版本名: $new_version_name"
-    sed -i '' "s/MARKETING_VERSION = $current_version_name/MARKETING_VERSION = $new_version_name/g" $p_file
-    exit 0
-elif [ "$name_type" == "minor" ]; then
-    current_version_name=$(grep -Eo 'MARKETING_VERSION = [0-9.]+' $p_file | grep -Eo '[0-9.]+' | tail -1)
-    echo "当前版本名: $current_version_name"
-    new_version_name=$(echo $current_version_name | awk -F. '{print $1"."$2+1".0"}')
-    echo "新版本名: $new_version_name"
-    sed -i '' "s/MARKETING_VERSION = $current_version_name/MARKETING_VERSION = $new_version_name/g" $p_file
-    exit 0
-elif [ "$name_type" == "patch" ]; then
-    current_version_name=$(grep -Eo 'MARKETING_VERSION = [0-9.]+' $p_file | grep -Eo '[0-9.]+' | tail -1)
-    echo "当前版本名: $current_version_name"
-    new_version_name=$(echo $current_version_name | awk -F. '{print $1"."$2"."$3+1}')
-    echo "新版本名: $new_version_name"
-    sed -i '' "s/MARKETING_VERSION = $current_version_name/MARKETING_VERSION = $new_version_name/g" $p_file
+if [ "$name_type" == "" ]; then
     exit 0
 fi
+current_version_name=$(grep -Eo 'MARKETING_VERSION = [0-9.]+' $p_file | grep -Eo '[0-9.]+' | tail -1)
+echo "当前版本名: $current_version_name"
+if [ "$name_type" == "major" ]; then
+    new_version_name=$(echo $current_version_name | awk -F. '{print $1+1".0.0"}')
+elif [ "$name_type" == "minor" ]; then
+    new_version_name=$(echo $current_version_name | awk -F. '{print $1"."$2+1".0"}')
+elif [ "$name_type" == "patch" ]; then
+    new_version_name=$(echo $current_version_name | awk -F. '{print $1"."$2"."$3+1}')
+fi
+echo "新版本名: $new_version_name"
+sed -i '' "s/MARKETING_VERSION = $current_version_name/MARKETING_VERSION = $new_version_name/g" $p_file
